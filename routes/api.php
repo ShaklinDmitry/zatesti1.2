9f5319b4ef2f17aee78b294e6afb4999b62bdffb
d.shaklin <d.shaklin@telecontact.ru>
2022-05-23 19:29:16 +0300

    -Сделал отправку сообщения в telegram

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use NotificationChannels\Telegram\TelegramUpdates;
-
+use NotificationChannels\Telegram\TelegramMessage;
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,4 +22,47 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/statements', [\App\Http\Controllers\StatementController::class, 'createStatement']);
 Route::get('/statements', [\App\Http\Controllers\StatementController::class, 'getStatements']);
-Route::delete('statements',[\App\Http\Controllers\StatementController::class, 'deleteStatement']);
+Route::delete('/statements',[\App\Http\Controllers\StatementController::class, 'deleteStatement']);
+
+
+Route::get('/sendtouser', function(){
+
+    $user = \App\Models\User::find(1);
+
+    Auth::login($user);
+    $user = auth()->user();
+  //  dd($user);
+    $user->notify(new \App\Notifications\TelegramNotification('new message'));
+});
+
+Route::get('telegram', function () {
+    $updates = TelegramUpdates::create()
+        // (Optional). Get's the latest update. NOTE: All previous updates will be forgotten using this method.
+        // ->latest()
+
+        // (Optional). Limit to 2 updates (By default, updates starting with the earliest unconfirmed update are returned).
+        ->limit(2)
+
+        // (Optional). Add more params to the request.
+        ->options([
+            'timeout' => 0,
+        ])
+        ->get();
+
+ //   return $updates;
+
+    if($updates['ok']) {
+        // Chat ID
+        $chatId = $updates['result'][0]['message']['chat']['id'];
+
+       // dd($chatId);
+
+        return TelegramMessage::create()
+            // Optional recipient user id.
+            ->to($chatId)
+            // Markdown supported.
+            ->content("Hello there!");
+
+        //     return $chatId;
+    }
+});
