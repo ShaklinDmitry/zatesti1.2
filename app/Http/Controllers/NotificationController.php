356d1718a6e5600a61060c70b59dff3fd85c23fc
d.shaklin <d.shaklin@telecontact.ru>
2022-05-31 14:07:17 +0300

    -Доделал до конца перевод на eloqent отправки высказываний

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -19,21 +19,11 @@ class NotificationController extends Controller
         Auth::login($user);
         $user = auth()->user();
 
-//        $statement = DB::select(
-//            "select * from statement where send_date_time = '1970-01-01 00:00:00' limit 1"
-//        );
-
         $statementModel = new Statement();
         $statement = $statementModel->getNotSendedStatement();
-      //  dd($statement->getNotSendedStatement());
 
         $user->notify(new \App\Notifications\TelegramNotification($statement->text));
 
         $statement->markSendedStatement($statement->id);
-
-//        DB::update(
-//            'update statement set `send_date_time` = NOW() where id = ?',
-//            [$statement[0]->id]
-//        );
     }
 }
