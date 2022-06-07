646452939a4a4c184c18fc702f82e2a30ffe3ee6
d.shaklin <d.shaklin@telecontact.ru>
2022-06-07 17:22:42 +0300

    -Написал тест для поиска неотправленных сообщений

--- tests/Feature/StatementNotificationTest.php
+++ tests/Feature/StatementNotificationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Statement;
 use App\Models\User;
 use App\Notifications\TelegramNotification;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -25,5 +26,36 @@ class StatementNotificationTest extends TestCase
 
         Notification::assertSentTo($user, TelegramNotification::class);
     }
+
+
+    /**
+     * тестирование поиска пользователя для отправки высказывания
+     * @return void
+     */
+    public function test_find_user_for_sending_statement(){
+
+    }
+
+
+    /**
+     * Тестирование получения неотправленных сообщений
+     * @return void
+     */
+    public function test_getting_not_sended_statement(){
+        $this->artisan('migrate:fresh');
+
+        $response = $this->post('/api/statements',
+            ['text' => "new statement for testing send statement"]);
+
+        $statement = new Statement();
+        $notSendedStatement = $statement->getNotSendedStatement();
+
+        $this->assertNotNull(
+            $notSendedStatement
+        );
+    }
+
+
+
 
 }
